a6437eca9f6a8b7ed2f58fe0889ce7b4b78412e4
seoyeong-4811 <tjdud9213@naver.com>
2024-10-31 09:52:52 +0900

    feat: 일정, 댓글 crud

---
+++ src/test/java/com/sparta/sheduleapp/SheduleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sheduleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheduleappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
